1138f01eec856aab24b8242b86687c7d3a8aeba3
sujit34 <sujitcsecuet@gmail.com>
2020-12-21 12:50:38 +0600

    Dependency Injection Autofac

---
+++ Dependency Injection/DIAutofac/DIAutofac/Dependency/DependencyRegister.cs
@@ -0,0 +1,18 @@
+﻿using Autofac;
+using DIAutofac.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DIAutofac.Dependency
+{
+    public class DependencyRegister : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            //builder.RegisterType<TestService>().As<ITestService>().InstancePerLifetimeScope();
+            builder.RegisterType<TestService>().As<ITestService>().SingleInstance();
+        }
+    }
+}
---
+++ Dependency Injection/DIAutofac/DIAutofac/Service/TestService.cs
@@ -0,0 +1,33 @@
+﻿using DIAutofac.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DIAutofac.Service
+{
+    public class TestService : ITestService
+    {
+        public List<TestModel> GettestList()
+        {
+            List<TestModel> list = new List<TestModel>
+            {
+                 new TestModel { Id = 1, Name = "Test1" },
+                 new TestModel { Id = 1, Name = "Test2" },
+                 new TestModel { Id = 1, Name = "Test3" },
+                 new TestModel { Id = 1, Name = "Test4" },
+                 new TestModel { Id = 1, Name = "Test5" },
+                 new TestModel { Id = 1, Name = "Test6" },
+                 new TestModel { Id = 1, Name = "Test7" },
+                 new TestModel { Id = 1, Name = "Test8" },
+                 new TestModel { Id = 1, Name = "Test9" },
+                 new TestModel { Id = 1, Name = "Test10" },
+                 new TestModel { Id = 1, Name = "Test11" },
+                 new TestModel { Id = 1, Name = "Test12" }
+
+            };
+
+            return list;           
+        }
+    }
+}
